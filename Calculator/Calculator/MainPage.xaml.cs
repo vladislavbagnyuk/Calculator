597abd9dca4ff7509df342065b188a3b278a4bf5
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-10-07 17:56:44 +0200

    Numbers and backspace

--- Calculator/Calculator/MainPage.xaml.cs
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -14,5 +14,25 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void onNumberButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            inputEditor.Text += button.Text;
+        }
+
+        private void onBackspaceButtonClicked(object sender, EventArgs args)
+        {
+            string s = inputEditor.Text;
+            if (s.Length > 1)
+            {
+                s = s.Substring(0, s.Length - 1);
+            }
+            else
+            {
+                s = "0";
+            }
+            inputEditor.Text = s;
+        }
     }
 }
