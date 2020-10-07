6ea6bd86c37e353da89e150a20b5e236b7460910
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-10-07 16:57:19 +0200

    Disabled keyboard on input focus

--- Calculator/Calculator.Android/Renderers/CustomEditorRenderer.cs
+++ Calculator/Calculator.Android/Renderers/CustomEditorRenderer.cs
@@ -1,20 +1,61 @@
-﻿using Android.Content;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
 using Calculator.Droid.Renderers;
 using Calculator.CustomControls;
 using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms;
+using Android.Views.InputMethods;
 
 [assembly: ExportRenderer(typeof(CustomEditor), typeof(CustomEditorRenderer))]
 namespace Calculator.Droid.Renderers
 {
     public class CustomEditorRenderer : EditorRenderer
     {
+
         public CustomEditorRenderer(Context context) : base(context) {}
 
         protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
         {
             base.OnElementChanged(e);
             Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+
+            if (e.NewElement != null)
+            {
+                (e.NewElement).PropertyChanging += OnPropertyChanging;
+            }
+
+            if (e.OldElement != null)
+            {
+                (e.OldElement).PropertyChanging -= OnPropertyChanging;
+            }
+
+            // Disable the Keyboard on Focus
+            this.Control.ShowSoftInputOnFocus = false;
+
+        }
+
+        private void OnPropertyChanging(object sender, PropertyChangingEventArgs propertyChangingEventArgs)
+        {
+            // Check if the view is about to get Focus
+            if (propertyChangingEventArgs.PropertyName == VisualElement.IsFocusedProperty.PropertyName)
+            {
+                // incase if the focus was moved from another Entry
+                // Forcefully dismiss the Keyboard 
+                InputMethodManager imm = (InputMethodManager)this.Context.GetSystemService(Context.InputMethodService);
+                imm.HideSoftInputFromWindow(this.Control.WindowToken, 0);
+            }
         }
     }
+
+
 }
