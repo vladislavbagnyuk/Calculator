6067e4de428dba2a21b26384b8e891b96fb7a0a4
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-10-10 17:41:03 +0200

    Added IOS renderer

---
+++ Calculator/Calculator.iOS/Renderers/CustomEditorRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Calculator.CustomControls;
+using Calculator.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEditor), typeof(CustomEditorRenderer))]
+
+namespace Calculator.iOS.Renderers
+{
+    public class CustomEditorRenderer : EditorRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            // Disabling the keyboard
+            this.Control.InputView = new UIView();
+        }
+    }
+}
